f8cabf03ec535f6fa79ecfbbaafec30eedfc192e
Luke Kozak <kozakluke@gmail.com>
2018-11-09 20:18:04 +0100

    Add select to navigate between samples

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Route, Switch} from 'react-router';
+import {Redirect, Route, Switch} from 'react-router';
 import {HashRouter} from 'react-router-dom';
 import {AudioPlayerSample} from '../../pages/audio-player-sample';
 import {CrossfadeBgSample} from '../../pages/crossfade-bg-sample';
@@ -11,13 +11,25 @@ export default function Content()
 {
     return (
         <HashRouter>
-            <Switch>
-                <Route path="/audio-player" component={AudioPlayerSample}/>
-                <Route path="/crossfade-bg" component={CrossfadeBgSample}/>
-                <Route path="/dialog" component={DialogSample}/>
-                <Route path="/responsive-image" component={ResponsiveImageSample}/>
-                <Route path="/spinner" component={SpinnerSample}/>
-            </Switch>
+            <>
+                <select
+                    onChange={(event)=>
+                        window.location.href = event.target.value}>
+                    <option value="#/audio-player">Audio Player</option>
+                    <option value="#/crossfade-bg">Crossfade Bg</option>
+                    <option value="#/dialog">Dialog</option>
+                    <option value="#/responsive-image">Responsive Image</option>
+                    <option value="#/spinner">Spinner</option>
+                </select>
+                <Switch>
+                    <Redirect from="/" to='/audio-player' exact/>
+                    <Route path="/audio-player" component={AudioPlayerSample}/>
+                    <Route path="/crossfade-bg" component={CrossfadeBgSample}/>
+                    <Route path="/dialog" component={DialogSample}/>
+                    <Route path="/responsive-image" component={ResponsiveImageSample}/>
+                    <Route path="/spinner" component={SpinnerSample}/>
+                </Switch>
+            </>
         </HashRouter>
     );
 }
