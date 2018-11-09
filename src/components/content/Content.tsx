b7ef3e1141ffae4b3cf7ed07b65126a5be3c7e92
Luke Kozak <kozakluke@gmail.com>
2018-11-09 19:39:25 +0100

    Clean content component

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,36 +1,23 @@
 import * as React from 'react';
-import * as style from './Content.pcss';
-import {PureComponent} from 'react';
-import {SpinnerSample} from '../../pages/spinner-sample';
+import {Route, Switch} from 'react-router';
+import {HashRouter} from 'react-router-dom';
 import {AudioPlayerSample} from '../../pages/audio-player-sample';
 import {CrossfadeBgSample} from '../../pages/crossfade-bg-sample';
+import {DialogSample} from '../../pages/dialog-sample';
 import {ResponsiveImageSample} from '../../pages/responsive-image-sample';
-import DialogSample from '../../pages/dialog-sample/DialogSample';
-import {HashRouter} from 'react-router-dom';
-import {Route, Switch} from 'react-router';
+import {SpinnerSample} from '../../pages/spinner-sample';
 
-export default class Content extends PureComponent<Props>
+export default function Content()
 {
-    onClick = ()=> {
-        console.log(this.props.message);
-    };
-    
-    render()
-    {
-        return (
-            <HashRouter>
-                <Switch>
-                    <Route path="/audio-player" component={AudioPlayerSample}/>
-                    <Route path="/crossfade-bg" component={CrossfadeBgSample}/>
-                    <Route path="/dialog" component={DialogSample}/>
-                    <Route path="/responsive-image" component={ResponsiveImageSample}/>
-                    <Route path="/spinner" component={SpinnerSample}/>
-                </Switch>
-            </HashRouter>
-        );
-    }
-}
-
-interface Props {
-    message:string;
+    return (
+        <HashRouter>
+            <Switch>
+                <Route path="/audio-player" component={AudioPlayerSample}/>
+                <Route path="/crossfade-bg" component={CrossfadeBgSample}/>
+                <Route path="/dialog" component={DialogSample}/>
+                <Route path="/responsive-image" component={ResponsiveImageSample}/>
+                <Route path="/spinner" component={SpinnerSample}/>
+            </Switch>
+        </HashRouter>
+    );
 }
