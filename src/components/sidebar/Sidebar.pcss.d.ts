e656b81339d5d5cccf957875d87ca13d34214fdb
Luke Kozak <kozakluke@gmail.com>
2018-11-16 09:15:53 +0100

    Create sidebar

---
+++ src/components/sidebar/Sidebar.pcss.d.ts
@@ -0,0 +1,5 @@
+export const legend:string;
+export const active:string;
+export const title:string;
+export const close:string;
+export const image:string;
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import * as style from './Sidebar.pcss';
+import {classNames} from '@kozakl/utils';
+
+export default function Sidebar(props:Props)
+{
+    const legendSidebarClass = classNames(
+        style.legend,
+        props.active && style.active
+    );
+    return (
+        <div className={legendSidebarClass}>
+            <h5 className={style.title}>Legenda</h5>
+            <a className={style.close} onClick={props.onClose}>
+                &#xe903;
+            </a>
+            Sidebar
+        </div>
+    );
+}
+
+interface Props {
+    active?:boolean;
+    onClose:()=> void;
+}
