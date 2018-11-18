09714aa398a30d5910014aeabbeae0e91c11fa17
Luke Kozak <kozakluke@gmail.com>
2018-11-18 18:18:10 +0100

    Update spinner size in sample

--- src/pages/spinner-sample/SpinnerSample.tsx
+++ src/pages/spinner-sample/SpinnerSample.tsx
@@ -7,7 +7,7 @@ export default function SpinnerSample()
     return (
         <div className={style.spinnerSample}>
             <Spinner color="red" size="1.5em"/>
-            <Spinner background="white"/>
+            <Spinner background="white" size="2rem"/>
             <Spinner/>
         </div>
     );
