63c9206075580838beb85483029cd29eb795fadb
Luke Kozak <kozakluke@gmail.com>
2018-11-25 11:08:14 +0100

    Create image-preview

---
+++ src/components/image-preview/ImagePreview.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import * as style from './ImagePreview.pcss';
+import {PureComponent} from 'react';
+import {scaleDown} from '@kozakl/utils';
+import {ResponsiveImage} from '../responsive-image';
+import {Image} from '../../types';
+
+export default class ImagePreview extends PureComponent<Props, State>
+{
+    constructor(props:Props)
+    {
+        super(props);
+        
+        this.state = {
+            current: props.current
+        };
+    }
+    
+    onClickPrev = ()=>
+    {
+        if (this.state.current > 0)
+            this.setState({current: this.state.current - 1});
+        else
+            this.setState({current: this.props.images.length - 1});
+    };
+    
+    onClickNext = ()=>
+    {
+        if (this.state.current < this.props.images.length - 1)
+            this.setState({current: this.state.current + 1});
+        else 
+            this.setState({current: 0});
+    };
+    
+    render()
+    {
+        const image = this.props.images[this.state.current];
+        const scale = scaleDown(
+            image.width / image.height,
+            window.innerWidth,
+            window.innerHeight
+        );
+        return (
+            <div className={style.imagePreview} style={{width: `${scale.x * 0.85}px`}}>
+                <a className={style.close} onClick={this.props.onClose}>
+                    <img src={require('res/icons/close.svg')}/>
+                </a>
+                <div className={style.imageContainer}>
+                    <ResponsiveImage
+                        ratio={image.height / image.width}
+                        thumb={`${image.path}_thumb.jpg`}
+                        srcSet={`${image.path}_366w.jpg 366w,
+                                 ${image.path}_480w.jpg 480w,
+                                 ${image.path}_676w.jpg 676w,
+                                 ${image.path}_732w-2x.jpg 732w,
+                                 ${image.path}_960w-2x.jpg 960w,
+                                 ${image.path}_1352w-2x.jpg 1352w`}
+                        sizes="1352px"/>
+                    <div className={style.arrows}>
+                        <a className={style.prev} onClick={this.onClickPrev}>
+                            <img src={require('res/prev.svg')}/>
+                        </a>
+                        <a className={style.next} onClick={this.onClickNext}>
+                            <img src={require('res/next.svg')}/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+interface Props {
+    images:Image[];
+    current:number;
+    onClose?:()=> void;
+}
+
+interface State {
+    current:number;
+}
---
+++ src/types/Image.ts
@@ -0,0 +1,5 @@
+export interface Image {
+    path:string;
+    width:number;
+    height:number;
+}
