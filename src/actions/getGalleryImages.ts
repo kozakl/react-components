52195a2c69d534f9dedf694481cb41cdd78e596f
Luke Kozak <kozakluke@gmail.com>
2018-12-01 13:43:34 +0100

    Add getGalleryImages action

---
+++ src/actions/getGalleryImages.ts
@@ -0,0 +1,18 @@
+import {ErrorResult, Image} from '../types';
+
+export function getGalleryImages():Promise<Image[]>
+{
+    return fetch('/gallery.json')
+        .then((response)=> response.json())
+        .then((result:Result)=> {
+            if (result.error)
+                throw result.error;
+            return result;
+        })
+        .catch((error)=> {
+            alert(error);
+            throw error;
+        });
+}
+
+interface Result extends Array<Image>, ErrorResult { }
