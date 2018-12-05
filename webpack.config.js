2f4abf463efcbba65b1d11a93f1fa440c988b002
Luke Kozak <kozakluke@gmail.com>
2018-12-05 13:28:51 +0100

    Disable source-map

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
             }
         ]
     },
-    devtool: 'source-map',
+    //devtool: 'source-map',
     devServer: {
         contentBase: 'public/',
         historyApiFallback: true,
