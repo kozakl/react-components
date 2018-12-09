6bea78e58d2f19d33bb0759a33f4166157145559
Luke Kozak <kozakluke@gmail.com>
2018-12-09 23:43:45 +0100

    Refactor Input as functional component instead class component

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,21 +1,27 @@
 import * as React from 'react';
 import * as style from './Input.pcss';
 import {classNames} from '@kozakl/utils';
+import {InputHTMLAttributes} from "react";
 
-export default class Input extends React.Component
+export default function Input(props:Props)
 {
-    render()
-    {
-        const {name, type, placeholder, onChange} = this.props;
-        const fieldClass = classNames(
-            style.field,
-            this.props.error && style.error
-        );
-        return (
-            <div className={this.props.className}>
-                <input className={fieldClass} {...{name, type, placeholder, onChange}}/>
-                {this.props.error && <div className={style.error}>{this.props.error}</div>}
-            </div>
-        )
-    }
+    const {name, type, placeholder, onChange} = props;
+    const fieldClass = classNames(
+        style.field,
+        props.error && style.error
+    );
+    return (
+        <div className={props.className}>
+            <input className={fieldClass} {...{name, type, placeholder, onChange}}/>
+            {props.error &&
+                <div className={style.error}>
+                    {props.error}
+                </div>}
+        </div>
+    )
+}
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+    className?:string;
+    error?:string;
 }
