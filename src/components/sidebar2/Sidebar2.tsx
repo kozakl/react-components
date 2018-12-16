fad31929c73af34763607d8696af155e1749f509
Luke Kozak <kozakluke@gmail.com>
2018-12-16 13:32:41 +0100

    Validate name and tel fields

--- src/components/sidebar2/Sidebar2.tsx
+++ src/components/sidebar2/Sidebar2.tsx
@@ -3,6 +3,7 @@ import * as style from './Sidebar2.pcss';
 import {ChangeEvent, FormEvent,
         MouseEvent, PureComponent} from 'react';
 import {classNames} from '@kozakl/utils';
+import {isFill} from '@kozakl/utils/validate';
 import {Checkbox} from '../checkbox';
 import {Input} from '../input';
 import {Modal} from '../modal';
@@ -26,18 +27,26 @@ export default class Sidebar2 extends PureComponent<Props, State>
     onSubmit = (event:FormEvent<HTMLFormElement>)=> {
         event.preventDefault();
         
-        this.setState({
-            errorName: 'Prosze podaj imie',
-            errorTel: 'Prosze podaj telefon'
-        });
+        if (!isFill(this.state.name)) {
+            this.setState({errorName: 'Prosze podaj imie'});
+        }
+        if (!isFill(this.state.tel)) {
+            this.setState({errorTel: 'Prosze podaj telefon'});
+        } else if (this.state.tel.length != 9) {
+            this.setState({errorTel: 'Nie poprawny numer'});
+        }
     };
     
     onChangeName = (event:ChangeEvent<HTMLInputElement>)=> {
         this.setState({name: event.target.value});
     };
     
-    onChangeTel = (event:ChangeEvent<HTMLInputElement>)=> {
-        this.setState({tel: event.target.value});
+    onChangeTel = (event:ChangeEvent<HTMLInputElement>)=>
+    {
+        const tel = event.target.value.trim();
+        if (!isNaN(+tel) && tel.length <= 9) {
+            this.setState({tel});
+        }
     };
     
     onClickTerms = (event:MouseEvent)=> {
@@ -78,6 +87,7 @@ export default class Sidebar2 extends PureComponent<Props, State>
                         value={this.state.tel}
                         error={this.state.errorTel}
                         onChange={this.onChangeTel}
+                        type="tel"
                         placeholder="Telefon (9 cyfr)"/>
                     
                     <Checkbox className={style.statement}>
