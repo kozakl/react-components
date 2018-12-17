1eea2d65d8aef17f925f7812b24e17b0dc70fcdc
Luke Kozak <kozakluke@gmail.com>
2018-12-17 11:00:05 +0100

    Update input to FunctionalComponent

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,66 +1,43 @@
 import * as React from 'react';
 import * as style from './Input.pcss';
-import {InputHTMLAttributes, PureComponent} from 'react';
+import {FunctionComponent,
+        InputHTMLAttributes} from 'react';
 import {classNames} from '@kozakl/utils';
 
-export default class Input extends PureComponent<Props, State>
+const Input:FunctionComponent<Props> = (props)=>
 {
-    constructor(props:Props)
-    {
-        super(props);
-        
-        this.state = {
-            focus: false
-        };
-    }
-    
-    onFocus = ()=> {
-        this.setState({focus: true});
-    };
-    
-    onBlur = ()=> {
-        this.setState({focus: false});
-    };
-    
-    render()
-    {
-        const inputClass = classNames(
-            this.props.className,
-            style.input
-        );
-        const textClass = classNames(
-            style.text,
-            this.props.error && style.error
-        );
-        const placeholderClass = classNames(
-            style.placeholder,
-            this.state.focus && style.open,
-            this.props.value &&
-            this.props.value.length && style.open
-        );
-        return (
-            <div
-                className={inputClass}
-                onFocus={this.onFocus}
-                onBlur={this.onBlur}>
-                <input
-                    className={textClass}
-                    type={this.props.type}
-                    value={this.props.value}
-                    onChange={this.props.onChange}/>
-                
-                {this.props.placeholder &&
-                    <div className={placeholderClass}>
-                        {this.props.placeholder}
-                    </div>}
-                {this.props.error &&
-                    <div className={style.error}>
-                        {this.props.error}
-                    </div>}
-            </div>
-        );
-    }
-}
+    const inputClass = classNames(
+        props.className,
+        style.input
+    );
+    const textClass = classNames(
+        style.text,
+        props.error && style.error
+    );
+    const placeholderClass = classNames(
+        style.placeholder,
+        props.value &&
+        props.value.length && style.open
+    );
+    return (
+        <div className={inputClass}>
+            <input
+                className={textClass}
+                type={props.type}
+                value={props.value}
+                onChange={props.onChange}/>
+            
+            {props.placeholder &&
+                <div className={placeholderClass}>
+                    {props.placeholder}
+                </div>}
+            {props.error &&
+                <div className={style.error}>
+                    {props.error}
+                </div>}
+        </div>
+    );
+};
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
     className?:string;
@@ -68,6 +45,4 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
     error?:string;
 }
 
-interface State {
-    focus:boolean;
-}
+export default Input;
