ab8192f181366c4885f816fc8c6759e875737c0e
Luke Kozak <kozakluke@gmail.com>
2018-12-30 18:01:21 +0100

    Add indicator to carousel and apply padding offset

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -16,24 +16,49 @@ export default class Carousel extends PureComponent<Props, State>
             next: null,
             loadedNext: false
         };
+        
+        window.addEventListener('resize', this.onResize);
+    }
+    
+    componentDidMount()
+    {
+        
     }
     
     onClick = (event:MouseEvent)=>
     {
-        this.list.scrollLeft = 500;
-        console.log(this.props.children)
+        const offset = (this.list.scrollWidth - this.state.paddingLeft - this.state.paddingRight) /
+            (this.props.children.length ) * (Number(event.target.id) );
+        
+        this.list.scrollLeft = offset;
+    };
+    
+    onResize = (event:MouseEvent)=>
+    {
+        const paddingLeft = this.list.clientWidth * 0.5 -
+                            this.list.firstChild.clientWidth * 0.5,
+              paddingRight = this.list.clientWidth * 0.5 -
+                             this.list.lastChild.clientWidth * 0.5;
+        this.list.style.paddingLeft = paddingLeft + 'px';
+        this.list.style.paddingRight = paddingRight + 'px';
     };
     
     render()
     {
+        console.log('render', this.list);
+        
         return (
-            <div onClick={this.onClick}>
+            <div>
                 <div
                     className={style.list}
                     ref={(list)=> this.list = list}>
                     {this.props.children}
                 </div>
-                
+                <div className={style.indicator} onClick={this.onClick}>
+                    {Children.map(this.props.children, (child, index)=> {
+                        return <span className={style.indicatorDot} id={index.toString()}/>
+                    })}
+                </div>
             </div>
         );
     }
--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -2,6 +2,7 @@ import {Redirect, Route, Switch} from 'react-router';
 import {HashRouter} from 'react-router-dom';
 import {AudioPlayerSample} from '../../pages/audio-player-sample';
 import {BadgeSample} from '../../pages/badge-sample';
+import {Carousel} from '../../components/carousel';
 import {CheckboxSample} from '../../pages/checkbox-sample';
 import {CrossfadeBgSample} from '../../pages/crossfade-bg-sample';
 import {DialogSample} from '../../pages/dialog-sample';
@@ -20,48 +21,12 @@ import React from 'react';
 export default function Content()
 {
     return (
-        <HashRouter>
-            <>
-                <select
-                    style={{position: 'fixed', top: '0'}}
-                    defaultValue={location.hash}
-                    onChange={(event)=>
-                        window.location.href = event.target.value}>
-                    <option value="#/audio-player">Audio Player</option>
-                    <option value="#/badge">Badge</option>
-                    <option value="#/checkbox">Checkbox</option>
-                    <option value="#/crossfade-bg">Crossfade Bg</option>
-                    <option value="#/dialog">Dialog</option>
-                    <option value="#/image-preview">Image Preview</option>
-                    <option value="#/input">Input</option>
-                    <option value="#/login">Login</option>
-                    <option value="#/modal">Modal</option>
-                    <option value="#/nav">Nav</option>
-                    <option value="#/responsive-image">Responsive Image</option>
-                    <option value="#/sidebar">Sidebar</option>
-                    <option value="#/sidebar2">Sidebar2</option>
-                    <option value="#/spinner">Spinner</option>
-                    <option value="#/window1">Window1</option>
-                </select>
-                <Switch>
-                    <Redirect from="/" to='/audio-player' exact/>
-                    <Route path="/audio-player" component={AudioPlayerSample}/>
-                    <Route path="/badge" component={BadgeSample}/>
-                    <Route path="/checkbox" component={CheckboxSample}/>
-                    <Route path="/checkbox" component={CrossfadeBgSample}/>
-                    <Route path="/dialog" component={DialogSample}/>
-                    <Route path="/image-preview" component={ImagePreviewSample}/>
-                    <Route path="/input" component={InputSample}/>
-                    <Route path="/login" component={LoginSample}/>
-                    <Route path="/modal" component={ModalSample}/>
-                    <Route path="/nav" component={NavSample}/>
-                    <Route path="/responsive-image" component={ResponsiveImageSample}/>
-                    <Route path="/sidebar" component={SidebarSample}/>
-                    <Route path="/sidebar2" component={Sidebar2Sample}/>
-                    <Route path="/spinner" component={SpinnerSample}/>
-                    <Route path="/window1" component={Window1Sample}/>
-                </Switch>
-            </>
-        </HashRouter>
+        <Carousel>
+            <div>HTML</div>
+            <div>CSS</div>
+            <div>JavaScript</div>
+            <div>xxxx</div>
+            <div>PHP</div>
+        </Carousel>
     );
 }
