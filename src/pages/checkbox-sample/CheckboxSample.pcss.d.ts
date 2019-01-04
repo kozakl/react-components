f1a05cbf54f86cbe382ea9df8ba5dcfceeac55fe
Luke Kozak <kozakluke@gmail.com>
2019-01-04 15:06:34 +0100

    Refactor checkbox-sample

--- src/pages/checkbox-sample/CheckboxSample.pcss.d.ts
+++ src/pages/checkbox-sample/CheckboxSample.pcss.d.ts
@@ -1,2 +1,5 @@
 export const checkboxSample:string;
-export const checkbox:string;
+export const checkbox1:string;
+export const checkbox2:string;
+export const checkbox3:string;
+export const checkbox4:string;
