cbe6de3028f10359cd7c4d351d282858a85859eb
Luke Kozak <kozakluke@gmail.com>
2019-03-13 20:58:54 +0100

    Refactor DialogSample to hook API

--- src/pages/dialog-sample/DialogSample.tsx
+++ src/pages/dialog-sample/DialogSample.tsx
@@ -1,90 +1,67 @@
-import {PureComponent} from 'react';
+import {useState} from 'react';
 import {Button} from 'reactstrap';
 import {Dialog} from '../../components/dialog';
 import {Modal} from '../../components/modal';
 import React from 'react';
 import style from './DialogSample.pcss';
 
-export default class DialogSample extends PureComponent<{}, State>
+export default function DialogSample()
 {
-    constructor()
-    {
-        super(undefined);
-        
-        this.state = {
-            infoDialog: false,
-            deleteDialog: false
-        };
+    const [infoDialog, setInfoDialog] = useState(false);
+    const [deleteDialog, setDeleteDialog] = useState(false);
+    
+    function onConfirmInfo() {
+        setInfoDialog(false);
     }
     
-    onClickInfo = ()=> {
-        this.setState({infoDialog: true});
-    };
-    
-    onClickDelete = ()=> {
-        this.setState({deleteDialog: true});
-    };
-    
-    onConfirmInfo = ()=> {
-        this.setState({infoDialog: false});
-    };
-    
-    onDismissInfo = ()=> {
-        this.setState({infoDialog: false});
-    };
-    
-    onConfirmDelete = ()=> {
-        this.setState({deleteDialog: false});
-    };
-    
-    onDismissDelete = ()=> {
-        this.setState({deleteDialog: false});
-    };
-    
-    render()
-    {
-        return (
-            <div className={style.dialogSample}>
-                <Button
-                    className={style.info}
-                    onClick={this.onClickInfo}
-                    color="success"
-                    size="sm">
-                    Show Info Dialog
-                </Button>
-                <Button
-                    onClick={this.onClickDelete}
-                    color="danger"
-                    size="sm">
-                    Show Delete Dialog
-                </Button>
-                <Modal visible={this.state.infoDialog}>
-                    <Dialog
-                        onConfirm={this.onConfirmInfo}
-                        onDismiss={this.onDismissInfo}
-                        title="Info!">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et tortor id erat fermentum malesuada. Duis tincidunt nulla ipsum, et lobortis ante pretium nec.
-                        </p>
-                    </Dialog>
-                </Modal>
-                <Modal visible={this.state.deleteDialog}>
-                    <Dialog
-                        onConfirm={this.onConfirmDelete}
-                        onDismiss={this.onDismissDelete}
-                        confirm="Delete"
-                        title="Warning!">
-                        <p>
-                            Are you sure you want to delete?
-                        </p>
-                    </Dialog>
-                </Modal>
-            </div>
-        );
+    function onDismissInfo() {
+        setInfoDialog(false);
     }
-}
-
-interface State {
-    infoDialog:boolean;
-    deleteDialog:boolean;
+    
+    function onConfirmDelete() {
+        setDeleteDialog(false);
+    }
+    
+    function onDismissDelete() {
+        setDeleteDialog(false);
+    }
+    
+    return (
+        <div className={style.dialogSample}>
+            <Button
+                className={style.info}
+                onClick={()=> setInfoDialog(true)}
+                color="success"
+                size="sm">
+                Show Info Dialog
+            </Button>
+            <Button
+                onClick={()=> setDeleteDialog(true)}
+                color="danger"
+                size="sm">
+                Show Delete Dialog
+            </Button>
+            <Modal visible={infoDialog}>
+                <Dialog
+                    onConfirm={onConfirmInfo}
+                    onDismiss={onDismissInfo}
+                    title="Info!">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et tortor id erat fermentum malesuada. Duis tincidunt nulla ipsum, et lobortis ante pretium nec.
+                    </p>
+                </Dialog>
+            </Modal>
+            <Modal visible={deleteDialog}>
+                <Dialog
+                    onConfirm={onConfirmDelete}
+                    onDismiss={onDismissDelete}
+                    confirm="Delete"
+                    title="Warning!">
+                    <p>
+                        Are you sure you want to delete?
+                    </p>
+                </Dialog>
+            </Modal>
+        </div>
+    );
 }
