9af1a2851e247369e9a403822f4c8d455c0070a1
Luke Kozak <kozakluke@gmail.com>
2019-03-15 10:30:43 +0100

    Refactor map-sample

--- src/pages/map-sample/MapSample.pcss.d.ts
+++ src/pages/map-sample/MapSample.pcss.d.ts
@@ -1,5 +1,3 @@
-export const checkboxSample:string;
+export const mapSample:string;
 export const map:string;
 export const info:string;
-export const checkbox3:string;
-export const checkbox4:string;
--- src/pages/map-sample/MapSample.tsx
+++ src/pages/map-sample/MapSample.tsx
@@ -1,9 +1,8 @@
 import {PureComponent} from 'react';
+import {InfoWindow, Marker} from 'react-google-maps';
+import {Map} from '../../components/map';
 import React from 'react';
 import style from './MapSample.pcss';
-import Marker from "react-google-maps/lib/components/Marker";
-import InfoWindow from "react-google-maps/lib/components/InfoWindow";
-import {Map} from "../../components/map";
 
 export default class MapSample extends PureComponent<{}, State>
 {
@@ -12,14 +11,12 @@ export default class MapSample extends PureComponent<{}, State>
         super(undefined);
         
         this.state = {
-            marker: true,
-            checkbox2: false,
-            checkbox3: false
+            infoWindow: false
         };
     }
     
-    onToggleMarker = ()=> {
-        this.setState({marker: !this.state.marker});
+    onToggleInfo = ()=> {
+        this.setState({infoWindow: !this.state.infoWindow});
     };
     
     render()
@@ -30,38 +27,34 @@ export default class MapSample extends PureComponent<{}, State>
             streetViewControl: false
         };
         return (
-            <div className={style.checkboxSample}>
-                <Map
-                    className={style.map}
-                    options={options}
-                    zoom={15}
-                    defaultCenter={{lat: 51.1080513, lng: 17.0251989}}>
-                    <Marker
-                        position={{lat: 51.1098569, lng: 17.028251}}
-                        onClick={this.onToggleMarker}
-                        label={{text: 'A'}}
-                        title="Title Marker">
-                        {this.state.marker &&
-                            <InfoWindow onCloseClick={this.onToggleMarker}>
-                                <div className={style.info}>
-                                    <h6>Lublin downtown</h6>
-                                    Ruska 65<br/>
-                                    50-072 Wrocław<br/>
-                                    Poland<br/>
-                                    <a target="_blank" href="https://www.google.pl/maps?q=Ruska+65+50-072+Wroc%C5%82aw&um=1&ie=UTF-8&sa=X&ved=0ahUKEwiw4a2g6djdAhXioosKHcmqDbUQ_AUICigB">
-                                        View on Google Maps
-                                    </a>
-                                </div>
-                            </InfoWindow>}
-                    </Marker>
-                </Map>
-            </div>
+            <Map
+                className={style.map}
+                options={options}
+                zoom={15}
+                defaultCenter={{lat: 51.1080513, lng: 17.0251989}}>
+                <Marker
+                    position={{lat: 51.1098569, lng: 17.028251}}
+                    onClick={this.onToggleInfo}
+                    label={{text: 'A'}}
+                    title="Title Marker">
+                    {this.state.infoWindow &&
+                        <InfoWindow onCloseClick={this.onToggleInfo}>
+                            <div className={style.info}>
+                                <h6>Lublin downtown</h6>
+                                Ruska 65<br/>
+                                50-072 Wrocław<br/>
+                                Poland<br/>
+                                <a target="_blank" href="https://www.google.pl/maps?q=Ruska+65+50-072+Wroc%C5%82aw&um=1&ie=UTF-8&sa=X&ved=0ahUKEwiw4a2g6djdAhXioosKHcmqDbUQ_AUICigB">
+                                    View on Google Maps
+                                </a>
+                            </div>
+                        </InfoWindow>}
+                </Marker>
+            </Map>
         );
     }
 }
 
 interface State {
-    marker:boolean;
-    checkbox2:boolean;
-    checkbox3:boolean;
+    infoWindow:boolean;
 }
