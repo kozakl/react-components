b423bf17262c18e77dfe414d31abd8459da267a6
Luke Kozak <kozakluke@gmail.com>
2019-03-18 10:19:27 +0100

    Implement error handling in useCheckboxChange hook

--- src/hooks/useCheckboxChange.ts
+++ src/hooks/useCheckboxChange.ts
@@ -3,8 +3,11 @@ import {useState} from 'react';
 export function useCheckboxChange(initialChecked:boolean)
 {
     const [checked, setChecked] = useState(initialChecked);
+    const [error, setError] = useState();
     return {
         checked,
-        onChange: ()=> setChecked(!checked)
+        error,
+        onChange: ()=> setChecked(!checked),
+        setError: (error:string)=> setError(error)
     }
 }
