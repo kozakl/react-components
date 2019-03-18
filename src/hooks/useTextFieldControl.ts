9500cd4eeed68078823ca25eec4d4c0aeca0ffa7
Luke Kozak <kozakluke@gmail.com>
2019-03-18 10:58:29 +0100

    Implement error handling in useTextFieldControl hook

--- src/hooks/useTextFieldControl.ts
+++ src/hooks/useTextFieldControl.ts
@@ -3,8 +3,10 @@ import {ChangeEvent, useState} from 'react';
 export function useTextFieldControl(initialValue:string,
                                     validator?:(value:string)=> boolean) {
     const [value, setValue] = useState(initialValue);
+    const [error, setError] = useState();
     return {
         value,
+        error,
         onChange: (event:ChangeEvent<HTMLInputElement>)=> {
             if (validator) {
                 if (validator(event.target.value)) {
@@ -13,6 +15,7 @@ export function useTextFieldControl(initialValue:string,
             } else {
                 setValue(event.target.value);
             }
-        }
+        },
+        setError: (error:string)=> setError(error)
     }
 }
