5c7a22174217f81e0e4e416691e112dd11321eda
Luke Kozak <kozakluke@gmail.com>
2019-03-30 10:03:48 +0100

    Update text-field-sample

--- src/pages/text-field-sample/TextFieldSample.pcss.d.ts
+++ src/pages/text-field-sample/TextFieldSample.pcss.d.ts
@@ -1,3 +1,3 @@
 export const textFieldSample:string;
-export const textField1:string;
+export const textField:string;
 export const textField2:string;
--- src/pages/text-field-sample/TextFieldSample.tsx
+++ src/pages/text-field-sample/TextFieldSample.tsx
@@ -7,28 +7,49 @@ export default function TextFieldSample()
 {
     const textField1 = useTextFieldControl(''),
           textField2 = useTextFieldControl(''),
-          textField3 = useTextFieldControl('');
+          textField3 = useTextFieldControl(''),
+          textField4 = useTextFieldControl(''),
+          textField5 = useTextFieldControl(''),
+          textField6 = useTextFieldControl('');
     return (
         <div className={style.textFieldSample}>
             <TextField
-                className={style.textField1}
-                placeholder="Imie Imie Imie Imie Imie Imie"
+                className={style.textField}
+                placeholder="Lorem ipsum dolor sit amet consectetur adipiscing"
                 {...textField1}/>
             <TextField
-                className={style.textField2}
-                placeholder="Telefon (9 cyfr)"
-                endAdornment={'END'}
+                className={style.textField}
+                placeholder="Lorem ipsum dolor sit amet consectetur adipiscing"
+                startAdornment="$$$"
                 {...textField2}/>
             <TextField
-                className={style.textField3}
+                className={style.textField}
+                placeholder="Lorem ipsum dolor sit amet consectetur adipiscing"
+                endAdornment="USD"
+                {...textField3}/>
+            <TextField
+                className={style.textField}
+                placeholder="Lorem ipsum dolor sit amet consectetur adipiscing"
+                startAdornment="$$$"
+                endAdornment="USD"
+                {...textField4}/>
+            <TextField
+                className={style.textField}
+                placeholder="Lorem ipsum dolor sit amet consectetur adipiscing"
+                {...textField5}
+                error="Lorem ipsum dolor sit amet consectetur adipiscing"/>
+            
+            {/*<TextField
+                className={style.textField}
                 placeholder={
                     <div className={style.fieldPlaceholder}>
                         Miesieczny dochód netto w gospodarstwie domowym:
                         <img src={require('res/icons/question-mark.svg')}/>
                     </div>}
                 endAdornment={'END'}
+                error={'Error sxee here'}
                 open
-                {...textField3}/>
+                {...textField5}/>*/}
         </div>
     );
 }
