f4459499acc28c524d3f793114163020af5ecd74
Luke Kozak <kozakluke@gmail.com>
2019-04-01 11:30:58 +0200

    Add images prop to class version of crossfade-bg

--- src/components/crossfade-bg/CrossfadeBg.tsx
+++ src/components/crossfade-bg/CrossfadeBg.tsx
@@ -6,9 +6,6 @@ import style from './CrossfadeBg.pcss';
 
 export default class CrossfadeBg extends PureComponent<Props, State>
 {
-    private images = [
-        'assets/bg1/', 'assets/bg2/'
-    ];
     private nextDelay:number;
     private swapDelay:number;
     
@@ -29,7 +26,7 @@ export default class CrossfadeBg extends PureComponent<Props, State>
     
     nextImage = ()=>
     {
-        if (this.state.current < this.images.length - 1)
+        if (this.state.current < this.props.images.length - 1)
             this.setState({next: this.state.current + 1, loadedNext: false});
         else
             this.setState({next: 0, loadedNext: false});
@@ -64,20 +61,20 @@ export default class CrossfadeBg extends PureComponent<Props, State>
                 <ResponsiveImage
                     className={style.current}
                     ratio={1280 / 1920}
-                    thumb={`${this.images[this.state.current]}/thumb.jpg`}
-                    srcSet={`${this.images[this.state.current]}/992w.jpg 661w,
-                             ${this.images[this.state.current]}/1200w.jpg 960w,
-                             ${this.images[this.state.current]}/1366w.jpg 1366w,
-                             ${this.images[this.state.current]}/1920w.jpg 1920w`}/>
-                {this.images[this.state.next] &&
+                    thumb={`${this.props.images[this.state.current]}/thumb.jpg`}
+                    srcSet={`${this.props.images[this.state.current]}/992w.jpg 661w,
+                             ${this.props.images[this.state.current]}/1200w.jpg 960w,
+                             ${this.props.images[this.state.current]}/1366w.jpg 1366w,
+                             ${this.props.images[this.state.current]}/1920w.jpg 1920w`}/>
+                {this.props.images[this.state.next] &&
                     <ResponsiveImage
                         className={nextClass}
                         ratio={1280 / 1920}
-                        thumb={`${this.images[this.state.next]}/thumb.jpg`}
-                        srcSet={`${this.images[this.state.next]}/992w.jpg 661w,
-                                 ${this.images[this.state.next]}/1200w.jpg 960w,
-                                 ${this.images[this.state.next]}/1366w.jpg 1366w,
-                                 ${this.images[this.state.next]}/1920w.jpg 1920w`}
+                        thumb={`${this.props.images[this.state.next]}/thumb.jpg`}
+                        srcSet={`${this.props.images[this.state.next]}/992w.jpg 661w,
+                                 ${this.props.images[this.state.next]}/1200w.jpg 960w,
+                                 ${this.props.images[this.state.next]}/1366w.jpg 1366w,
+                                 ${this.props.images[this.state.next]}/1920w.jpg 1920w`}
                         onLoadThumb={this.onLoadNext}/>}
             </div>
         );
@@ -86,6 +83,7 @@ export default class CrossfadeBg extends PureComponent<Props, State>
 
 interface Props {
     className?:string;
+    images:string[];
 }
 
 interface State {
