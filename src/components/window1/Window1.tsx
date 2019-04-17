aa8693bfea09979ab232cf5751a190421c762b7d
Luke Kozak <kozakluke@gmail.com>
2019-04-17 12:00:37 +0200

    Refactor window1 to hook version

--- src/components/window1/Window1.tsx
+++ src/components/window1/Window1.tsx
@@ -1,110 +1,90 @@
-import {ChangeEvent, FormEvent,
-        PureComponent} from 'react';
+import {
+    ChangeEvent, FormEvent, FunctionComponent, useState
+} from 'react';
 import {Button, CustomInput, Input} from 'reactstrap';
 import {isFill} from '@kozakl/utils/validate';
 import React from 'react';
 import style from './Window1.pcss';
+import {useTextField} from "@kozakl/hooks";
 
-export default class Window1 extends PureComponent<Props, State>
+const Window1:FunctionComponent<Props> = (props)=>
 {
-    public static defaultProps = {
-        categories1: [
-            'Category1-1', 'Category1-2', 'Category1-3', 'Category1-4'
-        ],
-        categories2: [
-            'Category2-1', 'Category2-2', 'Category2-3', 'Category2-4'
-        ]
-    };
+    const category1 = useSelect(1),
+          category2 = useSelect(1),
+          desc = useTextField('');
     
-    constructor(props:Props)
+    function onSubmit(event:FormEvent<HTMLFormElement>)
     {
-        super(props);
-        
-        this.state = {
-            category1: 1,
-            category2: 1,
-            desc: ''
-        };
-    }
-    
-    onSubmit = (event:FormEvent<HTMLFormElement>)=> {
         event.preventDefault();
-    };
-    
-    onChangeCategory1 = (event:ChangeEvent<HTMLInputElement>)=> {
-        this.setState({category1: +event.target.value});
-    };
-    
-    onChangeCategory2 = (event:ChangeEvent<HTMLInputElement>)=> {
-        this.setState({category2: +event.target.value});
-    };
-    
-    onChangeDesc = (event:ChangeEvent<HTMLInputElement>)=> {
-        this.setState({desc: event.target.value});
-    };
-    
-    render()
-    {
-        const enabled = isFill(this.state.desc);
-        return (
-            <div className={style.window1}>
-                <h5 className={style.title}>Window1</h5>
-                
-                <form className={style.form} onSubmit={this.onSubmit}>
-                    <label className={style.label} htmlFor="category1">Category1</label>
-                    <CustomInput
-                        className={style.category1}
-                        value={this.state.category1}
-                        onChange={this.onChangeCategory1}
-                        id="category1"
-                        type="select">
-                        {!this.state.category1 && <option>----</option>}
-                        {this.props.categories1.map((category, index)=>
-                            <option value={index} key={category}>
-                                {category}
-                            </option>)}
-                    </CustomInput>
-                    
-                    <label className={style.label} htmlFor="category2">Category2</label>
-                    <CustomInput
-                        className={style.category2}
-                        value={this.state.category2}
-                        onChange={this.onChangeCategory2}
-                        id="category2"
-                        type="select">
-                        {!this.state.category2 && <option>----</option>}
-                        {this.props.categories2.map((category, index)=>
-                            <option value={index} key={category}>
-                                {category}
-                            </option>)}
-                    </CustomInput>
-                    
-                    <label className={style.label} htmlFor="desc">Description</label>
-                    <Input
-                        className={style.desc}
-                        value={this.state.desc}
-                        onChange={this.onChangeDesc}
-                        id="desc"
-                        type="textarea"
-                        bsSize="sm"/>
-                    
-                    <div className={style.actions}>
-                        <Button onClick={this.props.onCancel} size="sm" outline>
-                            Cancel
-                        </Button>
-                        <Button
-                            disabled={!enabled}
-                            type="submit"
-                            color="success"
-                            size="sm">
-                            Add
-                        </Button>
-                    </div>
-                </form>
-            </div>
-        );
     }
-}
+    
+    const enabled = isFill(desc.value);
+    return (
+        <div className={style.window1}>
+            <h5 className={style.title}>Window1</h5>
+            
+            <form className={style.form} onSubmit={onSubmit}>
+                <label className={style.label} htmlFor="category1">Category1</label>
+                <CustomInput
+                    className={style.category1}
+                    value={category1.value}
+                    onChange={category1.onChange}
+                    id="category1"
+                    type="select">
+                    {props.categories1.map((category, index)=>
+                        <option value={index} key={category}>
+                            {category}
+                        </option>)}
+                </CustomInput>
+                
+                <label className={style.label} htmlFor="category2">Category2</label>
+                <CustomInput
+                    className={style.category2}
+                    value={category2.value}
+                    onChange={category2.onChange}
+                    id="category2"
+                    type="select">
+                    {!category2 && <option>----</option>}
+                    {props.categories2.map((category, index)=>
+                        <option value={index} key={category}>
+                            {category}
+                        </option>)}
+                </CustomInput>
+                
+                <label className={style.label} htmlFor="desc">Description</label>
+                <Input
+                    className={style.desc}
+                    value={desc.value}
+                    onChange={desc.onChange}
+                    id="desc"
+                    type="textarea"
+                    bsSize="sm"/>
+                
+                <div className={style.actions}>
+                    <Button onClick={props.onCancel} size="sm" outline>
+                        Cancel
+                    </Button>
+                    <Button
+                        disabled={!enabled}
+                        type="submit"
+                        color="success"
+                        size="sm">
+                        Add
+                    </Button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+Window1.defaultProps = {
+    categories1: [
+        'Category1-1', 'Category1-2', 'Category1-3', 'Category1-4'
+    ],
+    categories2: [
+        'Category2-1', 'Category2-2', 'Category2-3', 'Category2-4'
+    ]
+};
 
 interface Props {
     categories1?:string[];
@@ -113,8 +93,17 @@ interface Props {
     onCancel:()=> void;
 }
 
-interface State {
-    category1:number;
-    category2:number;
-    desc:string;
+export default Window1;
+
+function useSelect(initialValue:number)
+{
+    const [value, setValue] = useState(initialValue),
+          [error, setError] = useState();
+    return {
+        value,
+        error,
+        onChange: (event:ChangeEvent<HTMLInputElement>)=>
+            setValue(+event.target.value),
+        setError: (error:string)=> setError(error)
+    }
 }
