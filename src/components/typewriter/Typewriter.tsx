b4760706d1998ac516cf0f1177fab776adbf77fb
Luke Kozak <kozakluke@gmail.com>
2019-04-20 11:58:50 +0200

    Rewrite Typewrite to hook version

--- src/components/typewriter/Typewriter.tsx
+++ src/components/typewriter/Typewriter.tsx
@@ -1,54 +1,32 @@
-import {PureComponent} from 'react';
+import {FunctionComponent, useState} from 'react';
 import {classNames} from '@kozakl/utils';
 import React from 'react';
 import style from './Typewriter.pcss';
+import {useTimeout} from "@kozakl/hooks";
 
-export default class Typewriter extends PureComponent<Props, State>
+export const Typewriter:FunctionComponent<Props> = (props)=>
 {
-    public static defaultProps = {
-        delay: 0
-    };
-    private delay:number;
+    const [show, setShow] = useState(false);
     
-    constructor(props:Props)
-    {
-        super(props);
-        
-        this.state = {
-            start: false
-        };
-    }
+    useTimeout(()=> {
+        setShow(true);
+    }, props.delay);
     
-    componentDidMount()
-    {
-        this.delay = window.setTimeout(()=>
-            this.setState({start: true}), this.props.delay);
-    }
-    
-    componentWillUnmount() {
-        clearTimeout(this.delay);
-    }
-    
-    render()
-    {
-        const typewriterClass = classNames(
-            this.props.className,
-            style.typewriter,
-            this.state.start && style.show
-        );
-        return (
-            <div className={typewriterClass}>
-                {this.props.children}
-            </div>
-        );
-    }
-}
+    const typewriterClass = classNames(
+        props.className,
+        style.typewriter,
+        show && style.show
+    );
+    return (
+        <div className={typewriterClass}>
+            {props.children}
+        </div>
+    );
+};
 
 interface Props {
     className?:string;
     delay?:number;
 }
 
-interface State {
-    start:boolean;
-}
+export default Typewriter;
