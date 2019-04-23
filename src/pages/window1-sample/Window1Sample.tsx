f8f13fbaaf947b19303de4d331ec496f4b57c1b3
Luke Kozak <kozakluke@gmail.com>
2019-04-23 15:11:04 +0200

    Rewrite Window1Sample to hook version

--- src/pages/window1-sample/Window1Sample.tsx
+++ src/pages/window1-sample/Window1Sample.tsx
@@ -1,50 +1,26 @@
-import {PureComponent} from 'react';
+import {useState} from 'react';
 import {Button} from 'reactstrap';
 import {Modal} from '../../components/modal';
 import {Window} from '../../components/window1';
 import React from 'react';
 import style from './Window1Sample.pcss';
 
-export default class Window1Sample extends PureComponent<{}, State>
+export default function Window1Sample()
 {
-    constructor()
-    {
-        super(undefined);
-        
-        this.state = {
-            window: true
-        };
-    }
-    
-    onClickShowWindow = ()=> {
-        this.setState({window: true});
-    };
-    
-    onCancelWindow = ()=> {
-        this.setState({window: false});
-    };
-    
-    render()
-    {
-        return (
-            <div className={style.window1Sample}>
-                <Button
-                    className={style.show}
-                    onClick={this.onClickShowWindow}
-                    color="success">
-                    Show Window
-                </Button>
-                
-                <Modal visible={this.state.window} interactive>
-                    <Window
-                        onAdd={null}
-                        onCancel={this.onCancelWindow}/>
-                </Modal>
-            </div>
-        );
-    }
-}
-
-interface State {
-    window:boolean;
-}
+    const [window, setWindow] = useState(true);
+    return (
+        <div className={style.window1Sample}>
+            <Button
+                className={style.show}
+                onClick={()=> setWindow(true)}
+                color="success">
+                Show Window
+            </Button>
+            <Modal visible={window} interactive>
+                <Window
+                    onAdd={null}
+                    onCancel={()=> setWindow(false)}/>
+            </Modal>
+        </div>
+    );
+};
