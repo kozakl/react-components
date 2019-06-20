0451854c175ea98100a57c5f7ab4af43aff15788
Luke Kozak <kozakluke@gmail.com>
2019-06-20 23:02:04 +0200

    Add className prop to Dialog

--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -1,12 +1,17 @@
 import {FunctionComponent} from 'react';
 import {Button} from 'reactstrap';
+import {classNames} from '@kozakl/utils';
 import React from 'react';
 import style from './Dialog.pcss';
 
 const Dialog:FunctionComponent<Props> = (props)=>
 {
+    const dialogClass = classNames(
+        style.dialog,
+        props.className
+    );
     return (
-        <div className={style.dialog}>
+        <div className={dialogClass}>
             <h5 className={style.title}>
                 {props.title}
             </h5>
@@ -38,6 +43,7 @@ Dialog.defaultProps = {
 };
 
 interface Props {
+    className?:string;
     title:string;
     confirm?:string;
     dismiss?:string;
