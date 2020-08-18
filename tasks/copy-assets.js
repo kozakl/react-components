1c34081bd6fd66eec1507b4d7a178c140c264f32
Luke Kozak <kozakluke@gmail.com>
2020-08-18 14:45:41 +0200

    Add copy-assets task

---
+++ tasks/copy-assets.js
@@ -0,0 +1,6 @@
+import {copySync} from 'fs-extra';
+
+copySync('./assets/backgrounds/man-hd.mp4', './public/assets/backgrounds/man-hd.mp4');
+copySync('./assets/backgrounds/man-sd.mp4', './public/assets/backgrounds/man-sd.mp4');
+copySync('./assets/backgrounds/woman-hd.mp4', './public/assets/backgrounds/woman-hd.mp4');
+copySync('./assets/backgrounds/woman-sd.mp4', './public/assets/backgrounds/woman-sd.mp4');
