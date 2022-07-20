5ce4b73d2fda76dc5bb8f620fa41c4d70ab1ed9b
Luke Kozak <kozakluke@gmail.com>
2022-07-20 20:24:56 +0200

    Create calendar-popover component

---
+++ src/calendar-popover/CalendarPopover.tsx
@@ -0,0 +1,50 @@
+import {useState} from 'react';
+import {format, Locale} from 'date-fns';
+import {classNames} from '@kozakl/utils';
+import {Calendar} from '../calendar';
+import style from './CalendarPopover.module.css';
+
+const CalendarPopover = (props:Props)=>
+{
+    const [open, setOpen] = useState(false);
+    
+    return (
+        <div
+            className={classNames(
+                style.calendarPopover,
+                props.className
+            )}>
+            <div
+                className={classNames(
+                    style.date,
+                    props.disabled &&
+                        style.disabled
+                )}
+                onClick={()=>
+                    setOpen(!open)}>
+                {format(props.date, 'PP', {locale: props.locale})}
+            </div>
+            {!props.disabled &&
+                open &&
+                    <Calendar
+                        className={style.calendar}
+                        date={props.date}
+                        months={1}
+                        locale={props.locale}
+                        onChange={(date)=> {
+                            setOpen(false);
+                            props.onChange(date);
+                        }}/>}
+        </div>
+    );
+};
+
+interface Props {
+    className?:string;
+    date?:Date;
+    locale?:Locale;
+    disabled?:boolean;
+    onChange?:(date:Date)=> void;
+}
+
+export default CalendarPopover;
