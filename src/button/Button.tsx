2ed22d1103dd3557ef1249514d80aa8be8ca1770
Luke Kozak <kozakluke@gmail.com>
2022-07-21 12:01:53 +0200

    Create button component

---
+++ src/button/Button.tsx
@@ -0,0 +1,42 @@
+import {FunctionComponent, MouseEvent} from 'react';
+import {classNames} from '@kozakl/utils';
+import style from './Button.module.css';
+
+const Button:FunctionComponent<Props> = (props)=>
+{
+    const buttonClass = classNames(
+        style.button,
+        props.size == 'small' &&
+            style.small,
+        props.reversed && style.reversed,
+        props.className
+    );
+    return (
+        <button
+            className={buttonClass}
+            id={props.id}
+            type={props.type}
+            title={props.title}
+            disabled={props.disabled}
+            onClick={props.onClick}>
+            {props.children}
+        </button>
+    );
+};
+
+Button.defaultProps = {
+    type: 'button',
+};
+
+interface Props {
+    className?:string;
+    id?:string;
+    type?:'submit' | 'reset' | 'button';
+    size?:'normal' | 'small';
+    title?:string;
+    reversed?:boolean;
+    disabled?:boolean;
+    onClick?:(event:MouseEvent<HTMLButtonElement>)=> void;
+}
+
+export default Button;
