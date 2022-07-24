f1a8ea5bc3100a3161108d7899259ee51c441fa0
Luke Kozak <kozakluke@gmail.com>
2022-07-24 17:53:10 +0200

    Create toasts component

---
+++ src/toasts/Toasts.tsx
@@ -0,0 +1,20 @@
+import {Toast} from './toast';
+import {useToastsState} from '@kozakl/states';
+import React from 'react';
+import style from './Toasts.module.css';
+
+export const Toasts = ()=>
+{
+    const toasts = useToastsState().get();
+    return (
+        <div className={style.toasts}>
+            {toasts.map((toast)=>
+                <Toast
+                    className={style.toast}
+                    key={toast.id}
+                    {...toast}/>)}
+        </div>
+    );
+};
+
+export default Toasts;
