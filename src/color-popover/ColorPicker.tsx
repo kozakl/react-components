cdd80f0e476f930ed443b3615370ff7829626cc1
Luke Kozak <kozakluke@gmail.com>
2022-07-28 20:57:21 +0200

    Close color-popover when click outside

--- src/color-popover/ColorPicker.tsx
+++ src/color-popover/ColorPicker.tsx
@@ -1,19 +1,24 @@
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 import {HexColorPicker} from 'react-colorful';
+import {useClickOutside} from '@kozakl/hooks';
 import {classNames} from '@kozakl/utils';
 import React from 'react';
 import style from './ColorPicker.module.css';
 
-const ColorPicker = (props:Props)=>
-{
+const ColorPicker = (props:Props)=> {
+    const colorPopover = useRef();
     const [open, setOpen] = useState(false);
+    
+    useClickOutside(colorPopover, ()=>
+        setOpen(false));
     
     return (
         <div
             className={classNames(
                 style.colorPicker,
                 props.className
-            )}>
+            )}
+            ref={colorPopover}>
             <div
                 className={classNames(
                     style.swatch,
@@ -23,12 +28,13 @@ const ColorPicker = (props:Props)=>
                 style={{backgroundColor: props.color}}
                 onClick={()=>
                     setOpen(!open)}/>
-            {!props.disabled && open &&
-                <div className={style.popup}>
-                    <HexColorPicker
-                        color={props.color}
-                        onChange={props.onChange} />
-                </div>}
+            {!props.disabled &&
+                open &&
+                    <div className={style.popup}>
+                        <HexColorPicker
+                            color={props.color}
+                            onChange={props.onChange} />
+                    </div>}
         </div>
     );
 };
