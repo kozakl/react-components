8847d6e19959e117447795155e08f28b9c3a3f2b
Luke Kozak <kozakluke@gmail.com>
2022-08-14 11:48:02 +0200

    Update ChevronRight icon component

--- src/icons/chevron-right/ChevronRight.tsx
+++ src/icons/chevron-right/ChevronRight.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ChevronRight = (props:Props)=> (
     <svg
         className={props.className}
-        viewBox="0 0 25 42.2"
+        viewBox="0 0 18.3 32"
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
@@ -13,17 +13,20 @@ const ChevronRight = (props:Props)=> (
             height: props.height
         }}>
         <path
-            d="M0.5,38.3c0-0.4,0.2-0.6,0.4-1l16.2-16.2L0.9,4.9c-0.2-0.2-0.4-0.6-0.4-1s0.2-0.6,0.4-1l2-2c0.2-0.2,0.6-0.4,1-0.4s0.6,0.2,1,0.4l19.2,19.2c0.2,0.4,0.4,0.6,0.4,1s-0.2,0.6-0.4,1L4.9,41.3c-0.2,0.2-0.6,0.4-1,0.4s-0.6-0.2-1-0.4l-2-2C0.7,38.9,0.5,38.7,0.5,38.3z"
+            d="M2.3,32c0.6,0,1.2-0.2,1.6-0.7l13.7-13.7c0.9-0.9,0.9-2.3,0-3.2L3.9,0.7C3-0.2,1.6-0.2,0.7,0.7s-0.9,2.3,0,3.2L12.8,16L0.7,28.1c-0.9,0.9-0.9,2.3,0,3.2C1.1,31.8,1.7,32,2.3,32z"
             style={{
-                fill: '#FFF',
-                stroke: '#000',
-                strokeMiterlimit: 10
+                fill: props.color
             }}/>
     </svg>
 );
 
+ChevronRight.defaultProps = {
+    color: 'var(--color-primary)'
+}
+
 interface Props {
     className?:string;
+    color?:string;
     padding?:string;
     margin?:string;
     width?:string;
