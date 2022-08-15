7744f7b03034a3fcef952dfe51498d13d483b990
Luke Kozak <kozakluke@gmail.com>
2022-08-15 11:30:02 +0200

    Update ChevronLeft icon component

--- src/icons/chevron-left/ChevronLeft.tsx
+++ src/icons/chevron-left/ChevronLeft.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ChevronLeft = (props:Props)=> (
     <svg
         className={props.className}
-        viewBox="0 0 18.3 32"
+        viewBox="0 0 18.1 32"
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
@@ -12,11 +12,13 @@ const ChevronLeft = (props:Props)=> (
             width: props.width,
             height: props.height
         }}>
-        <path
-            d="M16,32c-0.6,0-1.2-0.2-1.6-0.7L0.7,17.6c-0.9-0.9-0.9-2.3,0-3.2L14.4,0.7c0.9-0.9,2.3-0.9,3.2,0s0.9,2.3,0,3.2L5.5,16l12.1,12.1c0.9,0.9,0.9,2.3,0,3.2C17.2,31.8,16.6,32,16,32z"
+        <polyline
             style={{
-                fill: props.color
-            }}/>
+                fill: 'none',
+                stroke: props.color,
+                strokeWidth: 3
+            }}
+            points="17,1 2,15.9 17,31"/>
     </svg>
 );
 
