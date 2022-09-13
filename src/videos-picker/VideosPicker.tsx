fec67dd5241717e7dfa6746ffa3c61076ebe96fa
Luke Kozak <kozakluke@gmail.com>
2022-09-13 20:05:10 +0200

    Add range slider to videos-picker

--- src/videos-picker/VideosPicker.tsx
+++ src/videos-picker/VideosPicker.tsx
@@ -1,10 +1,11 @@
 import {ChangeEvent, forwardRef} from 'react';
-import {VideoPicker} from '@kozakl/hooks';
-import {classNames} from '@kozakl/utils';
 import {IconButton} from '../icon-button';
 import {ChevronLeft, ChevronRight,
         CircleDownload, CircleMinus,
         CirclePlus} from '../icons';
+import {Range} from '../range';
+import {VideoPicker} from '@kozakl/hooks';
+import {classNames} from '@kozakl/utils';
 import React from 'react';
 import style from './VideosPicker.module.css';
 
@@ -27,70 +28,103 @@ const VideosPicker = forwardRef<HTMLInputElement, Props>((props, ref)=> {
             <div className={style.videos}>
                 {props.videos.map((video, index)=>
                     <div
-                        className={style.videoContainer}
+                        className={style.container}
                         key={video.file ?
                             video.name + video.file.size :
                             video.url}>
-                        <video
-                            className={style.video}
-                            title={video.name}
-                            src={video.file ?
-                                video.url :
-                                `${process.env.API}/${video.url}?w=152`}
-                            autoPlay
-                            muted
-                            loop/>
-                        {props.multiple &&
-                            <div className={style.arrows}>
-                                <IconButton
-                                    title="Move left"
-                                    active
-                                    disabled={
-                                        props.videos.length <= 1 ||
-                                        index <= 0}
-                                    onClick={()=>
-                                        props.moveLeft(index)}>
-                                    <ChevronLeft
+                        <div className={style.videoContainer}>
+                            <video
+                                className={style.video}
+                                style={{
+                                    aspectRatio: (video.width / video.height).toString()}}
+                                title={video.name}
+                                src={video.file ?
+                                    video.url :
+                                    `${process.env.API}/${video.url}?w=152`}
+                                onLoadedMetadata={(event)=> {
+                                    const target = event.target as HTMLVideoElement;
+                                    target.currentTime = video.start;
+                                    target.play();
+                                }}
+                                onTimeUpdate={(event)=> {
+                                    const target = event.target as HTMLVideoElement;
+                                    if (target.currentTime < video.start) {
+                                        target.currentTime = video.start;
+                                        target.play();
+                                    }
+                                    if (target.currentTime >= video.end - 0.2) {
+                                        target.currentTime = video.start;
+                                        target.play();
+                                    }
+                                }}
+                                muted/>
+                            {props.multiple &&
+                                <div className={style.arrows}>
+                                    <IconButton
+                                        title="Move left"
+                                        active
+                                        disabled={
+                                            props.videos.length <= 1 ||
+                                            index <= 0}
+                                        onClick={()=>
+                                            props.moveLeft(index)}>
+                                        <ChevronLeft
+                                            padding="0.25em"
+                                            height="100%"/>
+                                    </IconButton>
+                                    <IconButton
+                                        title="Move right"
+                                        active
+                                        disabled={
+                                            props.videos.length <= 1 ||
+                                            index >= props.videos.length - 1}
+                                        onClick={()=>
+                                            props.moveRight(index)}>
+                                        <ChevronRight
+                                            padding="0.25em"
+                                            height="100%"/>
+                                    </IconButton>
+                                </div>}
+                            {!video.file &&
+                                <a
+                                    className={style.download}
+                                    title="Download"
+                                    href={`${process.env.API}/${video.url}?download=true`}
+                                    download>
+                                    <CircleDownload
+                                        background="var(--background-primary)"
+                                        color="var(--color-primary)"
                                         padding="0.25em"
-                                        height="100%"/>
-                                </IconButton>
-                                <IconButton
-                                    title="Move right"
-                                    active
-                                    disabled={
-                                        props.videos.length <= 1 ||
-                                        index >= props.videos.length - 1}
-                                    onClick={()=>
-                                        props.moveRight(index)}>
-                                    <ChevronRight
-                                        padding="0.25em"
-                                        height="100%"/>
-                                </IconButton>
-                            </div>}
-                        {!video.file &&
-                            <a
-                                className={style.download}
-                                title="Download"
-                                href={`${process.env.API}/${video.url}?download=true`}
-                                download>
-                                <CircleDownload
+                                        width="1.875em"/>
+                                </a>}
+                            <IconButton
+                                className={style.remove}
+                                title="Remove"
+                                active
+                                onClick={()=>
+                                    props.remove(index)}>
+                                <CircleMinus
                                     background="var(--background-primary)"
                                     color="var(--color-primary)"
                                     padding="0.25em"
                                     width="1.875em"/>
-                            </a>}
-                        <IconButton
-                            className={style.remove}
-                            title="Remove"
-                            active
-                            onClick={()=>
-                                props.remove(index)}>
-                            <CircleMinus
-                                background="var(--background-primary)"
-                                color="var(--color-primary)"
-                                padding="0.25em"
-                                width="1.875em"/>
-                        </IconButton>
+                            </IconButton>
+                        </div>
+                        <div className={style.rangeContainer}>
+                            <Range
+                                values={[
+                                    video.start,
+                                    video.end
+                                ]}
+                                step={0.1}
+                                min={0}
+                                max={video.duration}
+                                onChange={(values)=>
+                                    props.cut(index, values[0], values[1])}/>
+                            <div className={style.rangeValues}>
+                                {video.start.toFixed(1)}–{video.end.toFixed(1)}
+                            </div>
+                        </div>
                     </div>)}
                 <label
                     className={classNames(
@@ -130,6 +164,7 @@ interface Props {
     error:string;
     moveLeft:(index:number)=> void;
     moveRight:(index:number)=> void;
+    cut:(index:number, start:number, end:number)=> void;
     remove:(index:number)=> void;
     onChange:(event:ChangeEvent<HTMLInputElement>)=> void;
 }
