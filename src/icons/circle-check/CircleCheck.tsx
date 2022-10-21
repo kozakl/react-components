e2eece2862772c330e94cac4c3faa5b993bda9c0
Luke Kozak <kozakluke@gmail.com>
2022-10-21 13:33:12 +0200

    Create circle-check icon component

---
+++ src/icons/circle-check/CircleCheck.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const CircleCheck = (props:Props)=> (
+    <svg
+        className={props.className}
+        viewBox={`
+            ${-1.5 * (props.outline - 1) / 2}
+            ${-1.5 * (props.outline - 1) / 2}
+            ${32 + 1.5 * (props.outline - 1)}
+            ${32 + 1.5 * (props.outline - 1)}`}
+        style={{
+            display: 'inline-block',
+            verticalAlign: 'middle',
+            padding: props.padding,
+            margin: props.margin,
+            width: props.width,
+            height: props.height
+        }}>
+        <circle
+            style={{
+                fill: props.background,
+                stroke: props.outline && props.colorSecondary,
+                strokeWidth: 1.5 * props.outline
+            }}
+            r="15.2"
+            cx="16"
+            cy="16"/>
+        <polyline
+            style={{
+                fill: props.background,
+                stroke: props.colorPrimary,
+                strokeWidth: 1.5 * props.stroke
+            }}
+            points="6.3,16 12.8,22.5 25.7,9.5"/>
+    </svg>
+);
+
+CircleCheck.defaultProps = {
+    background: 'none',
+    colorPrimary: 'var(--color-primary)',
+    colorSecondary: 'var(--color-primary)',
+    stroke: 1,
+    outline: 1
+}
+
+interface Props {
+    className?:string;
+    background?:string;
+    colorPrimary?:string;
+    colorSecondary?:string;
+    stroke?:number;
+    outline?:number;
+    padding?:string;
+    margin?:string;
+    width?:string;
+    height?:string;
+}
+
+export default CircleCheck;
