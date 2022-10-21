d5b4a7719bed2ade1f2ed4b5f2bfddd84aa9008a
Luke Kozak <kozakluke@gmail.com>
2022-10-21 13:45:08 +0200

    Update props in circle-minus

--- src/icons/circle-minus/CircleMinus.tsx
+++ src/icons/circle-minus/CircleMinus.tsx
@@ -15,8 +15,8 @@ const CircleMinus = (props:Props)=> (
         <circle
             style={{
                 fill: props.background,
-                stroke: props.color,
-                strokeWidth: 1.5 * props.stroke
+                stroke: props.outline && props.colorSecondary,
+                strokeWidth: 1.5 * props.outline
             }}
             r="15.2"
             cx="16"
@@ -24,7 +24,7 @@ const CircleMinus = (props:Props)=> (
 	    <line
             style={{
                 fill: 'none',
-                stroke: props.color,
+                stroke: props.colorPrimary,
                 strokeWidth: 1.5 * props.stroke
             }}
             x1="25.2"
@@ -36,15 +36,19 @@ const CircleMinus = (props:Props)=> (
 
 CircleMinus.defaultProps = {
     background: 'none',
-    color: 'var(--color-primary)',
-    stroke: 1
+    colorPrimary: 'var(--color-primary)',
+    colorSecondary: 'var(--color-primary)',
+    stroke: 1,
+    outline: 1
 }
 
 interface Props {
     className?:string;
     background?:string;
-    color?:string;
+    colorPrimary?:string;
+    colorSecondary?:string;
     stroke?:number;
+    outline?:number;
     padding?:string;
     margin?:string;
     width?:string;
