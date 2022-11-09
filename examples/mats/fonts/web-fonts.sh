a7fd024ab7aa99a7fb05fe055dc0c9af029f478f
Luke Kozak <kozakluke@gmail.com>
2022-11-09 15:09:37 +0100

    Add mats to examples

---
+++ examples/mats/fonts/web-fonts.sh
@@ -0,0 +1,11 @@
+pyftsubset Basier\ Square-Bold.otf \
+      --unicodes=U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD,U+0100-024F,U+0259,U+1E00-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF \
+      --layout-features='tnum' \
+      --output-file=../../public/fonts/basierSquare-bold.woff2 \
+      --flavor=woff2
+      
+pyftsubset Basier\ Square-Regular.otf \
+      --unicodes=U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD,U+0100-024F,U+0259,U+1E00-1EFF,U+2020,U+20A0-20AB,U+20AD-20CF,U+2113,U+2C60-2C7F,U+A720-A7FF \
+      --layout-features='tnum' \
+      --output-file=../../public/fonts/basierSquare-regular.woff2 \
+      --flavor=woff2
