420dec2afbbbffb721a4e5b4d008cd4130ccf200
Luke Kozak <kozakluke@gmail.com>
2022-11-14 23:12:45 +0100

    Add info about ColorPicker deprecated

--- src/color-popover/index.ts
+++ src/color-popover/index.ts
@@ -1,2 +1,4 @@
 export {default as ColorPopover} from './ColorPopover';
 export {default as ColorPicker} from './ColorPopover';
+
+console.log('ColorPicker is deprecated, use ColorPopover instead');
