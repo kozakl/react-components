85f5ab6d326bd600b3582b5a4c1d98006c51147a
Luke Kozak <kozakluke@gmail.com>
2022-11-14 15:00:59 +0100

    Add color-popover item to sidebar

--- examples/src/components/sidebar/Sidebar.tsx
+++ examples/src/components/sidebar/Sidebar.tsx
@@ -62,6 +62,13 @@ const Sidebar = (props:Props)=> {
             <NavLink
                 className={style.link}
                 activeClass={style.active}
+                href="/color-popover"
+                startWith>
+                Color Popover
+            </NavLink>
+            <NavLink
+                className={style.link}
+                activeClass={style.active}
                 href="/icons"
                 startWith>
                 Icons
