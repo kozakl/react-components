1c379c83d0ca1673befb563636ae8dc6b14bceee
Luke Kozak <kozakluke@gmail.com>
2022-12-06 21:10:48 +0100

    Implement theme props in Button and delete Button.module.css

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,16 +1,16 @@
 import {MouseEvent, PropsWithChildren} from 'react';
 import {classNames} from '@kozakl/utils';
 import React from 'react';
-import style from './Button.module.css';
 
 const Button = (props:Props)=> {
     return (
         <button
             className={classNames(
-                style.button,
                 props.size == 'small' &&
-                    style.small,
-                props.reversed && style.reversed,
+                    props.theme.small,
+                props.reversed &&
+                    props.theme.reversed,
+                props.theme.button,
                 props.className
             )}
             id={props.id}
@@ -24,11 +24,13 @@ const Button = (props:Props)=> {
 };
 
 Button.defaultProps = {
-    type: 'button',
+    theme: {},
+    type: 'button'
 };
 
 interface Props extends PropsWithChildren<{}> {
     className?:string;
+    theme?:any;
     id?:string;
     type?:'submit' | 'reset' | 'button';
     size?:'normal' | 'small';
