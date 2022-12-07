a95234f658a9b91d55c79d3ab6206f97985f44a6
Luke Kozak <kozakluke@gmail.com>
2022-12-07 22:28:23 +0100

    Add badge item to sidebar in example

--- examples/src/components/sidebar/Sidebar.tsx
+++ examples/src/components/sidebar/Sidebar.tsx
@@ -62,6 +62,13 @@ const Sidebar = (props:Props)=> {
             <NavLink
                 className={style.link}
                 activeClass={style.active}
+                href="/badge"
+                startWith>
+                Badge
+            </NavLink>
+            <NavLink
+                className={style.link}
+                activeClass={style.active}
                 href="/button"
                 startWith>
                 Button
