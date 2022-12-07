45d948ca10a7bf9b1e87a896afb0226404a4da62
Luke Kozak <kozakluke@gmail.com>
2022-12-07 14:19:31 +0100

    Create dialog route example

---
+++ examples/src/routes/dialog/Dialog.tsx
@@ -0,0 +1,45 @@
+import {useState} from 'react';
+import {Button} from '@kozakl/components/button';
+import {Dialog} from '@kozakl/components/dialog';
+import {useDialogsState} from '@kozakl/states';
+import {Layout} from '../../components/layout';
+import style from './Dialog.module.css';
+
+const Index = ()=> {
+    const [dialog, setDialog] = useState({visible: false});
+    const {createDialog} = useDialogsState();
+    
+    return (
+        <Layout title="Dialog - React Components">
+            <div className={style.dialog}>
+                <Button
+                    onClick={()=> 
+                        createDialog({
+                            title: 'Title',
+                            message: 'Message'
+                        })
+                    }>
+                    Dialog with hook
+                </Button>
+                <br/>
+                <br/>
+                <Button
+                    onClick={()=>
+                        setDialog({visible: true})}>
+                    Dialog standalone
+                </Button>
+                
+                <Dialog
+                    title="Title"
+                    message="Message"
+                    visible={dialog.visible}
+                    onConfirm={()=>
+                        setDialog({visible: false})}
+                    onDismiss={()=>
+                        setDialog({visible: false})}/>
+            </div>
+        </Layout>
+    );
+};
+
+export default Index;
