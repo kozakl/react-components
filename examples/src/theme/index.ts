8d1496957f1e8474a5a21b89c674aff0dda7fb23
Luke Kozak <kozakluke@gmail.com>
2022-12-07 23:08:37 +0100

    Add default badge theme in examples

--- examples/src/theme/index.ts
+++ examples/src/theme/index.ts
@@ -1,8 +1,10 @@
 import {themes} from '@kozakl/components/theme';
+import badge from '@kozakl/components/theme/badge.module.css';
 import button from '@kozakl/components/theme/button.module.css';
 import dialog from '@kozakl/components/theme/dialog.module.css';
 
 Object.assign(themes, {
+    badge,
     button,
     dialog
 });
