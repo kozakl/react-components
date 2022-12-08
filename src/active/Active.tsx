77f07ddc4db50bc80b3953d7b1bd84b1f12aa553
Luke Kozak <kozakluke@gmail.com>
2022-12-08 08:06:23 +0100

    Implement useTheme in Active

--- src/active/Active.tsx
+++ src/active/Active.tsx
@@ -1,25 +1,31 @@
 import {ChangeEventHandler} from 'react';
 import {classNames} from '@kozakl/utils';
+import {useTheme} from '../theme';
 import React from 'react';
 import style from './Active.module.css';
 
 const Active = (props:Props)=> {
+    const theme = useTheme('active');
     return (
         <label
             className={classNames(
                 style.active,
+                theme.active,
                 props.className
             )}
             title={props.title}>
             <input
-                className={style.input}
+                className={classNames(
+                    style.input,
+                    theme.input
+                )}
                 id={props.id}
                 type="checkbox"
                 name={props.name}
                 disabled={props.disabled}
                 checked={props.checked}
                 onChange={props.onChange}/>
-            <div className={style.content}/>
+            <div className={theme.content}/>
         </label>
     );
 };
