94b78cde318dd557b628dd41eac713a63b11743c
Luke Kozak <kozakluke@gmail.com>
2022-12-16 14:22:34 +0100

    Update default strokeWidth to 1.75 in Eye

--- src/icons/eye/Eye.tsx
+++ src/icons/eye/Eye.tsx
@@ -4,10 +4,10 @@ const Eye = (props:Props)=> (
     <svg
         className={props.className}
         viewBox={`
-            ${-1.5 * (props.stroke - 1) / 2}
-            ${-1.5 * (props.stroke - 1) / 2}
-            ${32 + 1.5 * (props.stroke - 1)}
-            ${32 + 1.5 * (props.stroke - 1)}`}
+            ${-1.75 * (props.stroke - 1) / 2}
+            ${-1.75 * (props.stroke - 1) / 2}
+            ${32 + 1.75 * (props.stroke - 1)}
+            ${32 + 1.75 * (props.stroke - 1)}`}
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
@@ -20,14 +20,14 @@ const Eye = (props:Props)=> (
             style={{
                 fill: 'none',
                 stroke: props.colorPrimary,
-                strokeWidth: 1.5 * props.stroke
+                strokeWidth: 1.75 * props.stroke
             }}
             d="M1.1,16c3,5.9,8.5,9.9,14.9,9.9c6.4,0,11.9-4,14.9-9.9c-3-5.9-8.5-9.9-14.9-9.9"/>
         <path
             style={{
                 fill: 'none',
                 stroke: props.colorPrimary,
-                strokeWidth: 1.5 * props.stroke
+                strokeWidth: 1.75 * props.stroke
             }}
             d="M23.2,16c0,4-3.2,7.3-7.2,7.3S8.8,20,8.8,16c0-4,3.2-7.3,7.2-7.3"/>
     </svg>
