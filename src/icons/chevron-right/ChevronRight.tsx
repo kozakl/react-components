785b6644dea4449bc256ca5c54136fe69d2f5363
Luke Kozak <kozakluke@gmail.com>
2023-01-18 19:00:57 +0100

    Add outline to ChevronRight icon

--- src/icons/chevron-right/ChevronRight.tsx
+++ src/icons/chevron-right/ChevronRight.tsx
@@ -4,10 +4,10 @@ const ChevronRight = (props:Props)=> (
     <svg
         className={props.className}
         viewBox={`
-            ${-2 * (props.stroke - 1) / 2}
-            ${-2 * (props.stroke - 1) / 2}
-            ${32 + 2 * (props.stroke - 1)}
-            ${32 + 2 * (props.stroke - 1)}`}
+            ${-2 * (props.outline || props.stroke - 1) / 2}
+            ${-2 * (props.outline || props.stroke - 1) / 2}
+            ${32 + 2 * (props.outline || props.stroke - 1)}
+            ${32 + 2 * (props.outline || props.stroke - 1)}`}
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
@@ -16,6 +16,14 @@ const ChevronRight = (props:Props)=> (
             width: props.width,
             height: props.height
         }}>
+        {props.outline &&
+            <polyline
+                style={{
+                    fill: 'none',
+                    stroke: props.colorSecondary,
+                    strokeWidth: 2 * props.outline
+                }}
+                points="8.1,1 23,15.9 8.1,31"/>}
         <polyline
             style={{
                 fill: 'none',
@@ -34,7 +42,9 @@ ChevronRight.defaultProps = {
 interface Props {
     className?:string;
     colorPrimary?:string;
+    colorSecondary?:string;
     stroke?:number;
+    outline?:number;
     padding?:string;
     margin?:string;
     width?:string;
