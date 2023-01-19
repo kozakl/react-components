dd2ec117edac0cf9cd55b63c1b4029c3c656596a
Luke Kozak <kozakluke@gmail.com>
2023-01-19 10:22:07 +0100

    Create carousel-transition component

---
+++ src/carousel-transition/CarouselTransition.tsx
@@ -0,0 +1,90 @@
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import {Children, cloneElement,
+        FunctionComponent, useState} from 'react';
+import {useLoad, useTimeout, useVisibility} from '@kozakl/hooks';
+import {classNames} from '@kozakl/utils';
+import {IconButton} from '../icon-button';
+import {useTheme} from '../theme';
+import React from 'react';
+import style from './CarouselTransition.module.css';
+
+const CarouselTransition:FunctionComponent<Props> = (props)=> {
+    const load = useLoad(),
+          theme = useTheme('carouselTransition'),
+          visibility = useVisibility();
+    const [elements, setElements] = useState({transition: {}, index: 0});
+    
+    useTimeout(()=> {
+        setElements({
+            transition: props.transitionNext,
+            index: (elements.index + 1) % children.length
+        });
+    },
+        load &&
+        visibility &&
+        props.speed,
+        [elements.index]);
+    
+    const children = Children.toArray(props.children);
+    return (
+        <div
+            className={classNames(
+                style.carouselTransition,
+                props.className
+            )}>
+            <div>
+                <div className={style.element}>
+                    <TransitionGroup
+                        childFactory={(child)=>
+                            cloneElement(child, {classNames: elements.transition})}>
+                        <CSSTransition<null>
+                            classNames={elements.transition}
+                            key={`${elements.index}`}
+                            addEndListener={(node:HTMLElement, complete:()=> void)=>
+                                node.addEventListener('transitionend', complete, false)}>
+                            {children[elements.index]}
+                        </CSSTransition>
+                    </TransitionGroup>
+                </div>
+            </div>
+            {children.length > 1 &&
+                <nav className={style.nav}>
+                    <IconButton
+                        className={style.prev}
+                        onClick={()=> {
+                            setElements({
+                                transition: props.transitionPrev,
+                                index: (elements.index - 1 + children.length) % children.length
+                            });
+                        }}>
+                        <props.iconPrev/>
+                    </IconButton>
+                    <IconButton
+                        className={style.next}
+                        onClick={()=> {
+                            setElements({
+                                transition: props.transitionNext,
+                                index: (elements.index + 1) % children.length
+                            });
+                        }}>
+                        <props.iconNext/>
+                    </IconButton>
+                </nav>}
+        </div>
+    );
+};
+
+CarouselTransition.defaultProps = {
+    speed: 5000
+};
+
+interface Props {
+    className?:string;
+    speed?:number;
+    transitionPrev:{};
+    transitionNext:{};
+    iconPrev:FunctionComponent;
+    iconNext:FunctionComponent;
+}
+
+export default CarouselTransition;
---
+++ src/carousel-transition/index.ts
@@ -0,0 +1,1 @@
+export {default as CarouselTransition} from './CarouselTransition';
