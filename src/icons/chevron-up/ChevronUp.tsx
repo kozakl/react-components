15cbe38635efe4f29e2fdf402db819eb4f493e3d
Luke Kozak <kozakluke@gmail.com>
2023-01-19 09:19:44 +0100

    Add outline to ChevronUp icon

--- src/icons/chevron-up/ChevronUp.tsx
+++ src/icons/chevron-up/ChevronUp.tsx
@@ -4,10 +4,10 @@ const ChevronUp = (props:Props)=> (
     <svg
         className={props.className}
         viewBox={`
-            ${-2 * (props.stroke - 1) / 2}
-            ${-2 * (props.stroke - 1) / 2}
-            ${32 + 2 * (props.stroke - 1)}
-            ${32 + 2 * (props.stroke - 1)}`}
+            ${-2 * (props.outline || props.stroke - 1) / 2}
+            ${-2 * (props.outline || props.stroke - 1) / 2}
+            ${32 + 2 * (props.outline || props.stroke - 1)}
+            ${32 + 2 * (props.outline || props.stroke - 1)}`}
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
@@ -16,6 +16,14 @@ const ChevronUp = (props:Props)=> (
             width: props.width,
             height: props.height
         }}>
+        {props.outline &&
+            <polyline
+                style={{
+                    fill: 'none',
+                    stroke: props.colorSecondary,
+                    strokeWidth: 2 * props.outline
+                }}
+                points="1,8 15.9,23 31,8"/>}
         <polyline
             style={{
                 fill: 'none',
@@ -34,7 +42,9 @@ ChevronUp.defaultProps = {
 interface Props {
     className?:string;
     colorPrimary?:string;
+    colorSecondary?:string;
     stroke?:number;
+    outline?:number;
     padding?:string;
     margin?:string;
     width?:string;
