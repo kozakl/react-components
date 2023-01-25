ce04b7169a888455fd51be7acac566387f478e70
Luke Kozak <kozakluke@gmail.com>
2023-01-25 11:51:57 +0100

    Update carousel example

--- examples/src/routes/carousel/Carousel.tsx
+++ examples/src/routes/carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import {Carousel} from '@kozakl/components/carousel';
+import {CarouselSlider} from '@kozakl/components/carousel-slider';
 import {ChevronLeft, ChevronRight} from '@kozakl/components/icons';
 import {Image} from '@kozakl/components/image';
 import {Layout} from '../../components/layout';
@@ -12,11 +12,11 @@ const Index = ()=> {
     return (
         <Layout title="Carousel - React Components">
             <div className={style.index}>
-                {/*<Carousel className={style.carousel1}>
+                <CarouselSlider className={style.carousel1}>
                     <div className={style.item}>A</div>
                     <div className={style.item}>B</div>
                     <div className={style.item}>C</div>
-                </Carousel>*/}
+                </CarouselSlider>
                 
                 <CarouselTransition
                     transitionPrev={fade}
@@ -45,8 +45,9 @@ const Index = ()=> {
                     <div className={style.x}>B</div>
                     <div className={style.x}>C</div>
                 </CarouselTransition>
-                {/*
-                <Carousel>
+                
+                <CarouselSlider
+                    dots>
                     <div className={style.card}>
                         <Image
                             className={style.cardImage}
@@ -106,7 +107,88 @@ const Index = ()=> {
                             </p>
                         </div>
                     </div>
-                </Carousel>*/}
+                </CarouselSlider>
+                
+                
+                <CarouselSlider
+                    className={style.carouselArrows}
+                    arrows
+                    
+                    moveBlock
+                    iconPrev={()=>
+                        <ChevronLeft
+                            colorPrimary="var(--color-primary)"
+                            colorSecondary="var(--color-secondary)"
+                            stroke={2}
+                            strokeLinecap="round"
+                            outline={3}/>}
+                    iconNext={()=>
+                        <ChevronRight
+                            colorPrimary="var(--color-primary)"
+                            colorSecondary="var(--color-secondary)"
+                            stroke={2}
+                            strokeLinecap="round"
+                            outline={3}/>}>
+                    <div className={style.card1}>
+                        <Image
+                            className={style.cardImage}
+                            thumb={`https://eu2allwebext.blob.core.windows.net/suburbansquare/www/resources/images/pages/default/i_home_about.jpg`}
+                            srcSet={`https://eu2allwebext.blob.core.windows.net/suburbansquare/www/resources/images/pages/default/i_home_about.jpg 800w`}/>
+                        <div className={style.cardBody}>
+                            <div className={style.cardTitle}>
+                                <h3>Decyzja kredytowa przed podpisaniem umowy zakupy</h3>
+                            </div>
+                            <p className={style.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                    
+                    <div className={style.card1}>
+                        <Image
+                            className={style.cardImage}
+                            thumb={`https://eu2allwebext.blob.core.windows.net/suburbansquare/www/resources/images/pages/default/i_home_about.jpg`}
+                            srcSet={`https://eu2allwebext.blob.core.windows.net/suburbansquare/www/resources/images/pages/default/i_home_about.jpg 800w`}/>
+                        <div className={style.cardBody}>
+                            <div className={style.cardTitle}>
+                                <h3>Ubezpieczenie na 5 lat</h3>
+                            </div>
+                            <p className={style.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                    
+                    <div className={style.card1}>
+                        <Image
+                            className={style.cardImage}
+                            thumb={`https://eu2allwebext.blob.core.windows.net/suburbansquare/www/resources/images/pages/default/i_home_about.jpg`}
+                            srcSet={`https://eu2allwebext.blob.core.windows.net/suburbansquare/www/resources/images/pages/default/i_home_about.jpg 800w`}/>
+                        <div className={style.cardBody}>
+                            <div className={style.cardTitle}>
+                                <h3>Unikatowy projekt wnętrz</h3>
+                            </div>
+                            <p className={style.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                    
+                    <div className={style.card1}>
+                        <Image
+                            className={style.cardImage}
+                            thumb={`https://eu2allwebext.blob.core.windows.net/suburbansquare/www/resources/images/pages/default/i_home_about.jpg`}
+                            srcSet={`https://eu2allwebext.blob.core.windows.net/suburbansquare/www/resources/images/pages/default/i_home_about.jpg 800w`}/>
+                        <div className={style.cardBody}>
+                            <div className={style.cardTitle}>
+                                <h3>Voucher na zakupy w Leroy Merlin</h3>
+                            </div>
+                            <p className={style.cardText}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                </CarouselSlider>
             </div>
         </Layout>
     );
