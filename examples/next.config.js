75bc6f01eb3757e9a945c5840d0ad1e13d76830a
Luke Kozak <kozakluke@gmail.com>
2023-03-16 09:48:08 +0100

    Use new transpilePackages api in next.config.js

--- examples/next.config.js
+++ examples/next.config.js
@@ -1,10 +1,10 @@
-const withTM = require('next-transpile-modules')([
-    '@kozakl/components',
-    '@kozakl/date-fns',
-    '@kozakl/hooks',
-    '@kozakl/states',
-    '@kozakl/transitions',
-    '@kozakl/utils'
-]);
-
-module.exports = withTM({});
+module.exports = {
+    transpilePackages: [
+        '@kozakl/components',
+        '@kozakl/date-fns',
+        '@kozakl/hooks',
+        '@kozakl/states',
+        '@kozakl/transitions',
+        '@kozakl/utils'
+    ]
+};
