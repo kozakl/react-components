e374b7b54582e8c4393a8fdc3e18ba56f936231f
Luke Kozak <kozakluke@gmail.com>
2023-04-17 13:01:59 +0200

    Update arrow-button-left component

--- src/icons/arrow-bottom-left/ArrowBottomLeft.tsx
+++ src/icons/arrow-bottom-left/ArrowBottomLeft.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 const ArrowBottomLeft = (props:Props)=> (
     <svg
         className={props.className}
-        viewBox={`
-            ${-2 * (props.stroke - 1) / 2}
-            ${-2 * (props.stroke - 1) / 2}
-            ${32 + 2 * (props.stroke - 1)}
-            ${32 + 2 * (props.stroke - 1)}`}
+        viewBox="0 0 32 32"
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
@@ -22,7 +18,7 @@ const ArrowBottomLeft = (props:Props)=> (
                 stroke: props.colorPrimary,
                 strokeWidth: 2 * props.stroke
             }}
-            d="M30,30H2 M2,30V2 M2,30L30,2"/>
+            d="M24,24H8 M8,24V8 M8,24L24,8"/>
     </svg>
 );
 
