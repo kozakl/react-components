6f9ff030be8338900f001a90b90021b5aa6c38f5
Luke Kozak <kozakluke@gmail.com>
2023-04-18 11:34:28 +0200

    Update arrow-down icon component

--- src/icons/arrow-down/ArrowDown.tsx
+++ src/icons/arrow-down/ArrowDown.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 const ArrowDown = (props:Props)=> (
     <svg
         className={props.className}
-        viewBox={`
-            ${-2 * (props.stroke - 1) / 2}
-            ${-2 * (props.stroke - 1) / 2}
-            ${32 + 2 * (props.stroke - 1)}
-            ${32 + 2 * (props.stroke - 1)}`}
+        viewBox="0 0 32 32"
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
@@ -20,18 +16,18 @@ const ArrowDown = (props:Props)=> (
             style={{
                 fill: 'none',
                 stroke: props.colorPrimary,
-                strokeWidth: 2 * props.stroke
+                strokeWidth: props.stroke
             }}
-            points="31.5,15.4 16.1,30.8 0.5,15.4"/>
+            points="24,15.4 16.1,23.4 8,15.4"/>
         <line
             style={{
                 stroke: props.colorPrimary,
-                strokeWidth: 2 * props.stroke
+                strokeWidth: props.stroke
             }}
             x1="16.1"
-            y1="0"
+            y1="7.5"
             x2="16.1"
-            y2="31"/>
+            y2="23.5"/>
     </svg>
 );
 
