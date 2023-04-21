e3a6654cc580c17148e8d1036d24ba432627078c
Luke Kozak <kozakluke@gmail.com>
2023-04-21 09:30:50 +0200

    Update xmark icon component

--- src/icons/xmark/XMark.tsx
+++ src/icons/xmark/XMark.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 const XMark = (props:Props)=> (
     <svg
         className={props.className}
-        viewBox={`
-            ${-2 * (props.stroke - 1) / 2}
-            ${-2 * (props.stroke - 1) / 2}
-            ${32 + 2 * (props.stroke - 1)}
-            ${32 + 2 * (props.stroke - 1)}`}
+        viewBox="0 0 32 32"
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
@@ -20,22 +16,22 @@ const XMark = (props:Props)=> (
             style={{
                 fill: 'none',
                 stroke: props.colorPrimary,
-                strokeWidth: 2 * props.stroke
+                strokeWidth: props.stroke
             }}
-            x1="1"
-            y1="31"
-            x2="31"
-            y2="1"/>
+            x1="8"
+            y1="24"
+            x2="24"
+            y2="8"/>
         <line
             style={{
                 fill: 'none',
                 stroke: props.colorPrimary,
-                strokeWidth: 2 * props.stroke
+                strokeWidth: props.stroke
             }}
-            x1="1"
-            y1="1"
-            x2="31"
-            y2="31"/>
+            x1="8"
+            y1="8"
+            x2="24"
+            y2="24"/>
     </svg>
 );
 
