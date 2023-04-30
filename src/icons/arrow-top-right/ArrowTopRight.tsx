d1cf9bde7b69f61fe52aa854f417ff770f9efdfb
Luke Kozak <kozakluke@gmail.com>
2023-04-30 21:22:49 +0200

    Add outline support in ArrowTopRight icon component

--- src/icons/arrow-top-right/ArrowTopRight.tsx
+++ src/icons/arrow-top-right/ArrowTopRight.tsx
@@ -7,11 +7,20 @@ const ArrowTopRight = (props:Props)=> (
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
+            strokeLinecap: props.strokeLinecap,
             padding: props.padding,
             margin: props.margin,
             width: props.width,
             height: props.height
         }}>
+        {props.outline &&
+            <path
+                style={{
+                    fill: 'none',
+                    stroke: props.colorSecondary,
+                    strokeWidth: props.outline
+                }}
+                d="M8,8h16 M24,8v16 M24,8L8,24"/>}
         <path
             style={{
                 fill: 'none',
@@ -30,7 +39,10 @@ ArrowTopRight.defaultProps = {
 interface Props {
     className?:string;
     colorPrimary?:string;
+    colorSecondary?:string;
     stroke?:number;
+    strokeLinecap?:'butt' | 'round' | 'square',
+    outline?:number;
     padding?:string;
     margin?:string;
     width?:string;
