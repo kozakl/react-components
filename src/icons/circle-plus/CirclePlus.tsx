95ead8c6c40cf7f8b33cf3dbc4edf7b2d490a6a7
Luke Kozak <kozakluke@gmail.com>
2023-05-15 19:15:36 +0200

    Update CirclePlus

--- src/icons/circle-plus/CirclePlus.tsx
+++ src/icons/circle-plus/CirclePlus.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 const CirclePlus = (props:Props)=> (
     <svg
         className={props.className}
-        viewBox={`
-            ${-2 * (props.outline - 1) / 2}
-            ${-2 * (props.outline - 1) / 2}
-            ${32 + 2 * (props.outline - 1)}
-            ${32 + 2 * (props.outline - 1)}`}
+        viewBox="0 0 32 32"
         style={{
             display: 'inline-block',
             verticalAlign: 'middle',
@@ -20,30 +16,30 @@ const CirclePlus = (props:Props)=> (
             style={{
                 fill: props.background,
                 stroke: props.outline && props.colorSecondary,
-                strokeWidth: 2 * props.outline
+                strokeWidth: props.outline
             }}
-            r="15"
+            r="8"
             cx="16"
             cy="16"/>
         <line
             style={{
                 fill: 'none',
                 stroke: props.colorPrimary,
-                strokeWidth: 2 * props.stroke
+                strokeWidth: props.stroke
             }}
             x1="16"
-            y1="6.8"
+            y1="11.1"
             x2="16"
-            y2="25.2"/>
+            y2="20.9"/>
 	    <line
             style={{
                 fill: 'none',
                 stroke: props.colorPrimary,
-                strokeWidth: 2 * props.stroke
+                strokeWidth: props.stroke
             }}
-            x1="25.2"
+            x1="20.9"
             y1="16"
-            x2="6.8"
+            x2="11.1"
             y2="16"/>
     </svg>
 );
