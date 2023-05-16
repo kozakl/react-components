a6fb606e216583e99b1b900b03b8492cadb890eb
Luke Kozak <kozakluke@gmail.com>
2023-05-16 15:01:01 +0200

    Update imports theme in examples

--- examples/src/theme/index.ts
+++ examples/src/theme/index.ts
@@ -2,24 +2,52 @@ import {themes} from '@kozakl/components/theme';
 import active from '@kozakl/components/theme/active.module.css';
 import badge from '@kozakl/components/theme/badge.module.css';
 import button from '@kozakl/components/theme/button.module.css';
+import calendarPopover from '@kozakl/components/theme/calendarPopover.module.css';
 import carouselSlider from '@kozakl/components/theme/carouselSlider.module.css';
 import carouselTransition from '@kozakl/components/theme/carouselTransition.module.css';
 import checkbox from '@kozakl/components/theme/checkbox.module.css';
+import colorPopover from '@kozakl/components/theme/colorPopover.module.css';
 import dialog from '@kozakl/components/theme/dialog.module.css';
+import iconButton from '@kozakl/components/theme/iconButton.module.css';
+import imagesPicker from '@kozakl/components/theme/imagesPicker.module.css';
+import loading from '@kozakl/components/theme/loading.module.css';
+import loadingModal from '@kozakl/components/theme/loadingModal.module.css';
 import modal from '@kozakl/components/theme/modal.module.css';
 import paginate from '@kozakl/components/theme/paginate.module.css';
+import range from '@kozakl/components/theme/range.module.css';
+import searchField from '@kozakl/components/theme/searchField.module.css';
+import textField from '@kozakl/components/theme/textField.module.css';
 import textRichArea from '@kozakl/components/theme/textRichArea.module.css';
+import toast from '@kozakl/components/theme/toast.module.css';
+import toasts from '@kozakl/components/theme/toasts.module.css';
+import toggleSwitch from '@kozakl/components/theme/toggleSwitch.module.css';
+import videosPicker from '@kozakl/components/theme/videosPicker.module.css';
+import vimeoPlayer from '@kozakl/components/theme/vimeoPlayer.module.css';
 import '@kozakl/components/theme/calendar.css';
 
 Object.assign(themes, {
     active,
     badge,
     button,
+    calendarPopover,
     carouselSlider,
     carouselTransition,
     checkbox,
+    colorPopover,
     dialog,
+    iconButton,
+    imagesPicker,
+    loading,
+    loadingModal,
     modal,
     paginate,
-    textRichArea
+    range,
+    searchField,
+    textField,
+    textRichArea,
+    toast,
+    toasts,
+    toggleSwitch,
+    videosPicker,
+    vimeoPlayer
 });
